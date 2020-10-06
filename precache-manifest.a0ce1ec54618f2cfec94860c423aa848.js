7f79b5965e0f3997086c06e26916331e90a2653f
Marcin <mzmuda627@gmail.com>
2020-10-06 17:06:23 +0200

    Updates

---
+++ precache-manifest.a0ce1ec54618f2cfec94860c423aa848.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e3a59b6c7d991aa4bac",
+    "url": "/react-spa-app/static/css/main.75489d83.chunk.css"
+  },
+  {
+    "revision": "8e3a59b6c7d991aa4bac",
+    "url": "/react-spa-app/static/js/main.a9ca4192.chunk.js"
+  },
+  {
+    "revision": "f35487bfb635674a419f",
+    "url": "/react-spa-app/static/js/runtime~main.48761d8f.js"
+  },
+  {
+    "revision": "e06ff08ce8035e1a1b2f",
+    "url": "/react-spa-app/static/js/2.ec904aca.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-spa-app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-spa-app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-spa-app/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "476aa24c83eefa4013548571e9b15503",
+    "url": "/react-spa-app/index.html"
+  }
+];
